333d59efce9140f06670219a3d62568f8d0e3ff7
Christian Daniel Ortiz Orobia <cortiz@sms-sudamerica.com>
2022-03-14 19:46:39 -0300

    feat | Change header text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Logo</h1>
+        <h1>Este es el branch test</h1>
       </header>
     </div>
   );
