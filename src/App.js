ac26a1f2477f218d197c8ae69eb7f370693c438a
Christian Daniel Ortiz Orobia <cortiz@sms-sudamerica.com>
2022-03-15 21:07:46 -0300

    Feat | Clase 4 - Componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
+import NavBar from './components/NavBar/NavBar'
+//import NavBarClass from './components/NavBarClass'
+import ListProducts from './components/ListProducts/ListProducts';
+import ModalCustom from './components/Modal/Modal'
+import Card from './components/Card/Card';
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (value) => {
+      setOpen(false);
+  };
+  
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
   return (
+    //JSX
     <div className="App">
-      <header className="App-header">
-        <h1>Logo</h1>
-      </header>
+     <NavBar />
+      {/* <NavBarClass /> */}
+      <ListProducts>
+        <p>Productos Relacionados</p>
+        <span>subtitulo</span>
+
+      </ListProducts>
+      <ListProducts>
+        <p>Productos en Oferta</p>
+      </ListProducts>
+      <button onClick={handleOpen}>Abrir modal</button>
+      <ModalCustom handleClose={handleClose} open={open}>
+        <p>FORMULARIO Contacto</p>
+      </ModalCustom>
+
+      <ModalCustom handleClose={handleClose} open={open}>
+        <Card />
+      </ModalCustom>
     </div>
   );
 }
---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+const CartWidget = () => {
+    return (
+        <div>
+            {/* <img src={} /> */}
+            ICONO DEL CARRITO
+            <p>4</p>
+        </div>
+    )
+}
+
+export default CartWidget
